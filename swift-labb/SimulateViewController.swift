8edd51c79ab7e6ec0fd1b1a8f60c93f08d93d7fe
Petar Mataic <petar.mataic@jayway.com>
2014-09-26 11:35:31 +0200

    Fixed some view controllers and storyboard connections

---
+++ swift-labb/SimulateViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SimulateViewController.swift
+//  swift-labb
+//
+//  Created by Petar Mataic on 26/09/14.
+//  Copyright (c) 2014 Petar Mataic. All rights reserved.
+//
+
+import UIKit
+
+class SimulateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
