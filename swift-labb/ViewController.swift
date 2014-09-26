c8388d3848c95e90b12070e9488d41da64047853
Petar Mataic <petar@Petars-MacBook-Pro-2.local>
2014-09-26 10:45:39 +0200

    Initial Commit

---
+++ swift-labb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-labb
+//
+//  Created by Petar Mataic on 26/09/14.
+//  Copyright (c) 2014 Petar Mataic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
